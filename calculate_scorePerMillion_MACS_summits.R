e261911b76e8e5469b042ac6ba476f5c286d215d
Austin Hepperla <hepperla@longleaf-login2.its.unc.edu>
2019-04-02 10:00:36 -0400

    Script that takes in a MACS2 summit.bed file and returns calculated score per million peak bed files that have all peaks, peaks with a score per million greater than -log10(3), and peaks with a score per million greater than -log10(5). The peak bed file returned will be the summit +-250bp.

---
+++ calculate_scorePerMillion_MACS_summits.R
@@ -0,0 +1,16 @@
+#script takes in a MACS2 summit file and converts it to a TCGA-like score-per-million bed file (summit +-250bp)
+args = commandArgs(TRUE)
+
+peaks = read.table(args[1], sep="\t")
+peaks.total.score = sum(peaks[,5])
+
+peaks[,5] = peaks[,5]/(peak.total.score/1000000)
+peaks[,1] = peaks[,1]-250
+peaks[,2] = peaks[,2]+250
+peaks[,6] = "+"
+
+out_basename = gsub("_summits.bed","", args[1], fixed=TRUE)
+
+write.out(peaks, file=paste0(out_basename,"_summit_plusMinus250_scorePerMillion.bed"), row.names=F, col.names=F, sep="\t", quote=F)
+write.out(peaks[peaks[,5] >= 3,], file=paste0(out_basename,"_summit_plusMinus250_scorePerMillion_greaterLog3SPM.bed"), row.names=F, col.names=F, sep="\t", quote=F)
+write.out(peaks[peaks[,5] >= 5,], file=paste0(out_basename,"_summit_plusMinus250_scorePerMillion_greaterLog5SPM.bed"), row.names=F, col.names=F, sep="\t", quote=F)
